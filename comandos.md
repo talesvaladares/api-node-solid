80748a22903bef738663c19f2b0fbaa14a72a54a
Tales Eduardo <tales.silva@psvsistemas.com.br>
2023-03-09 11:47:54 -0300

    criado a estrutura básica do projeto

---
+++ comandos.md
@@ -0,0 +1,8 @@
+- npm init -y 
+- npm i typescript @types/node tsx tsup -D
+  - npx tsc --init
+- npm install fastify
+- npm install dotenv
+- npm install zod
+- npm install dotenv
+- npm install @rocketseat/eslint-config -D
---
+++ src/env/index.ts
@@ -0,0 +1,18 @@
+import 'dotenv/config';
+import { z as zod } from 'zod';
+
+const envSchema = zod.object({
+  NODE_ENV: zod.enum(['dev', 'test', 'production']).default('dev'),
+  PORT: zod.coerce.number().default(3333),
+});
+
+const _env = envSchema.safeParse(process.env);
+
+if (!_env.success) {
+  console.error('Invalid environment variables', _env.error.format());
+
+  // se este erro acontecer, a aplicação será interrompida
+  throw new Error('Invalid environment variables.');
+}
+
+export const env = _env.data;
